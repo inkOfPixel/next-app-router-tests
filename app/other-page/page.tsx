d18bf342db482ed2a2a54cb041d6a707a80df701
Federico Badini <federico@inkofpixel.com>
2023-08-10 11:13:19 +0200

    feat: add code reference

--- app/other-page/page.tsx
+++ app/other-page/page.tsx
@@ -1,3 +1,4 @@
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
 import Link from "next/link";
 import { Suspense } from "react";
 import { Clock, ClockFallback } from "../_components/Clock";
@@ -5,7 +6,7 @@ import { Clock, ClockFallback } from "../_components/Clock";
 export default async function OtherPage() {
   return (
     <div className="flex flex-col px-16 py-24 w-10/12 items-center">
-      <h1 className="text-4xl text-lime-400">Other page</h1>
+      <h1 className="text-4xl text-green-400">Other page</h1>
       <div className="flex flex-col gap-4 mt-8 p-4 items-center border border-solid rounded-md">
         <Suspense fallback={<ClockFallback title="Cached clock" />}>
           <Clock title="Cached clock" />
@@ -14,6 +15,19 @@ export default async function OtherPage() {
       <Link className="text-sm text-white underline mt-8" href="/">
         Go to homepage
       </Link>
+      <div className="mt-8">
+        <a
+          href="https://github.com/inkOfPixel/next-app-router-tests/blob/main/app/other-page/page.tsx"
+          target="_blank"
+          className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          View source
+          <ArrowTopRightOnSquareIcon
+            className="-mr-0.5 h-5 w-5"
+            aria-hidden="true"
+          />
+        </a>
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import { Suspense } from "react";
 import { Clock, ClockFallback } from "./_components/Clock";
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
 
 export default async function Homepage() {
   return (
     <div className="flex flex-col px-16 py-24 w-10/12 items-center">
-      <h1 className="text-4xl text-lime-400">Homepage</h1>
+      <h1 className="text-4xl text-green-500">Homepage</h1>
       <div className="flex flex-col gap-4 mt-8 p-4 items-center border border-solid rounded-md">
         <Suspense fallback={<ClockFallback title="Cached clock" />}>
           <Clock title="Cached clock" />
@@ -38,6 +39,19 @@ export default async function Homepage() {
       <Link className="text-sm text-white underline mt-8" href="/other-page">
         Go to other page
       </Link>
+      <div className="mt-8">
+        <a
+          href="https://github.com/inkOfPixel/next-app-router-tests/blob/main/app/page.tsx"
+          target="_blank"
+          className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          View source
+          <ArrowTopRightOnSquareIcon
+            className="-mr-0.5 h-5 w-5"
+            aria-hidden="true"
+          />
+        </a>
+      </div>
     </div>
   );
 }
