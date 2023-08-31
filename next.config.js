c461460fa8dc40aba51e665dcfdc5e703db235ca
Federico Badini <federico@inkofpixel.com>
2023-08-31 16:37:08 +0200

    feat: testing caching headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/about",
+        headers: [
+          {
+            key: "x-custom-header",
+            value: "my custom header value",
+          },
+          {
+            key: "x-another-custom-header",
+            value: "my other custom header value",
+          },
+          {
+            key: "Cache-Control",
+            value: "max-age=60",
+          },
+          {
+            key: "CDN-Cache-Control",
+            value: "max-age=60",
+          },
+          {
+            key: "Vercel-CDN-Cache-Control",
+            value: "max-age=3600",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
